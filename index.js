60a57abd63be27e554d0c3eb0e647d6030eb2f6d
Cara <43040862+caraMcG@users.noreply.github.com>
2023-12-13 22:18:41 -0700

    Updated to require cors

--- index.js
+++ index.js
@@ -4,9 +4,12 @@ const PORT = process.env.PORT || 8000
 const express = require('express')
 const axios = require('axios')
 const cheerio = require('cheerio')
+const cors = require('cors')
 
 //call express
 const app = express()
+//set cors 
+app.use(cors())
 
 //dog profile variables
 const dogProfiles = []
